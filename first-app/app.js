c89782a54a1943599b80d399009eea838a505d3c
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-02 19:05:16 -0700

    Event module

--- first-app/app.js
+++ first-app/app.js
@@ -3,23 +3,50 @@
 // console.log(logger)
 // logger.log("message")
 
+// Path Module
+
 // const path = require('node:path');
 // var pathobj=path.parse(__filename);
 // console.log(pathobj);
 
+
+// OS module
 // const os = require('node:os');
 
 // console.log(os.totalmem())
 // console.log(os.freemem())
 
-const fs=require('fs');
-console.log(fs.readdirSync('./'));
+// File Module
+// const fs=require('fs');
+// console.log(fs.readdirSync('./'));
 
-console.log(fs.readdir('./',function(err,files){
-    if(err){
-        console.log('Error',err);
-    }
-    else{
-        console.log('Result',files);
-    }
-}))
+// console.log(fs.readdir('./',function(err,files){
+//     if(err){
+//         console.log('Error',err);
+//     }
+//     else{
+//         console.log('Result',files);
+//     }
+// }))
+
+// Event
+const EventEmitter = require('events')
+const emitter= new EventEmitter();
+
+// Register an listner
+emitter.on('MessageLogged',(arg)=>{
+    console.log('Listener Called',arg)
+})
+
+// Signaling a event has happend - Raise and event
+
+// emitter.emit('MessageLogged',{id:1,url:'https://'})
+
+
+
+
+
+
+
+
+
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,10 +1,17 @@
 var url='http://mylogger.io/log'
 console.log(__filename);
 console.log(__dirname);
+const EventEmitter=require('events');
+const emitter=new EventEmitter();
+emitter.on('logging',(message)=>{
+console.log('logging',message)
+})
 function log(message){
+    
     console.log(message)
+    emitter.emit('logging',message);
 }
-
+log('kausic');
 // let log=()=>{
 
 // }
