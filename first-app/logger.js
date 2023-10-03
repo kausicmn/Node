1d5daa034e610345db966dec1421b13b481f1bfd
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-02 18:30:56 -0700

    Node Modules

---
+++ first-app/logger.js
@@ -0,0 +1,12 @@
+var url='http://mylogger.io/log'
+console.log(__filename);
+console.log(__dirname);
+function log(message){
+    console.log(message)
+}
+
+// let log=()=>{
+
+// }
+
+module.exports.log=log; // export modules
