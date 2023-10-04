1f0d0f30f879e8efd19d0568101c74c6dc5c43a4
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-03 18:43:03 -0700

    Put method

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,6 +10,8 @@ const courses =[
     {id:5,name:'course5'}
 
 ]
+
+
 app.get('/',(req,res)=>{
     res.send('Hello world')
 });
@@ -25,6 +27,9 @@ res.send(course);
 // res.send(req.params);
 // res.send(req.query); //http://localhost:3001/api/courses/1?sortBy=name
 })
+
+
+
 app.post('/api/courses',(req,res)=>{
     const schema=Joi.object({
         name: Joi.string().min(3).required()
@@ -36,9 +41,10 @@ app.post('/api/courses',(req,res)=>{
     //     res.status(400).send('Name is required');
     //     return;
     // }
-    if(result.error)
+    const {error}=validatecourse(req.body);
+    if(error)
     {
-        res.status(400).send(result.error.details[0].message);
+        res.status(400).send(error.details[0].message);
         return;
     }
     const course={
@@ -48,5 +54,26 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 })
+
+app.put('/api/courses/:id',(req,res)=>{
+const course=courses.find(c=>c.id===parseInt(req.params.id));
+if(!course) res.status(404).send('The course with given ID was not found')
+
+const {error}=validatecourse(req.body);
+if(error)
+{
+    res.status(400).send(error.details[0].message);
+    return;
+}
+course.name=req.body.name
+res.send(course);
+})
+function validatecourse(course)
+{
+    const schema=Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course)
+}
 const port=process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listening on Port ${port}....`));
