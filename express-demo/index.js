1e9a86e470bd565c9ad12f8de5542a0ea3d23359
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-03 17:37:05 -0700

    Get method express

---
+++ express-demo/index.js
@@ -0,0 +1,28 @@
+var express=require('express')
+const app=express();
+const courses =[
+    {id:1,name:'course1'},
+    {id:2,name:'course2'},
+    {id:3,name:'course3'},
+    {id:4,name:'course4'},
+    {id:5,name:'course5'}
+
+]
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+});
+app.get('/api/courses',(req,res)=>{
+    res.send(courses);
+})
+app.get('/api/courses/:id',(req,res)=>{
+
+const course=courses.find(c=>c.id===parseInt(req.params.id));
+if(!course) res.status(404).send('The course with given ID was not found')
+res.send(course);
+
+ //res.send(req.params.id);
+// res.send(req.params);
+// res.send(req.query); //http://localhost:3001/api/courses/1?sortBy=name
+})
+const port=process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Listening on Port ${port}....`));
