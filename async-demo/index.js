6a53d6baef7d18e25c309f6fe8e43238b70f1113
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-07 18:09:30 -0700

    Modifying Promises to callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,28 +1,35 @@
-console.log('Before');
-getuser(1,displayuser); 
-console.log('After'); // 2 nd one has to wait until first one is executed. Synchrous program
+// console.log('Before');
+// getuser(1,displayuser)
+// console.log('After'); // 2 nd one has to wait until first one is executed. Synchrous program
 
-function displayuser(user){
-    console.log(user) 
-    getrepo(user.gitusername,displayrepo)
-}
-function displayrepo(repo){
-    console.log(repo)
-}
+// function displayuser(user){
+//     console.log(user) 
+//     getrepo(user.gitusername,displayrepo)
+// }
+// function displayrepo(repo){
+//     console.log(repo)
+// }
 
-function getuser(id,callback){
-    setTimeout(()=>{
-        console.log('Reading a user from DB') // Async non blocking function
-        callback({id:id,gitusername:'kausic'})
-        //return {id:id,gitusername:'kausic'}
-    },2000)
+getuser(1).then(user=>getrepo(user.gitusername)).then(repo=>console.log(repo)).catch(err=>console.log('Error',err.message));
+
+function getuser(id){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Reading a user from DB') // Async non blocking function
+            resolve({id:id,gitusername:'kausic'})
+            //return {id:id,gitusername:'kausic'}
+        },2000)
+    }) 
     
 }
 
-function getrepo(username,callback){
-    setTimeout(() => {
-        console.log('Getting repo information')
-        callback(['repo1','repo2','repo3'])
-    }, 2000);
+function getrepo(username){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log('Getting repo information')
+            resolve(['repo1','repo2','repo3'])
+        }, 2000);
+    })
+    
    
 }
