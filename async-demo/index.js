f11e19903e3b77173d32fd3f008fa3b17e0e865d
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-07 19:25:14 -0700

    Async await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,3 +1,5 @@
+// Call back
+
 // console.log('Before');
 // getuser(1,displayuser)
 // console.log('After'); // 2 nd one has to wait until first one is executed. Synchrous program
@@ -10,9 +12,29 @@
 //     console.log(repo)
 // }
 
-getuser(1).then(user=>getrepo(user.gitusername)).then(repo=>console.log(repo)).catch(err=>console.log('Error',err.message));
+// Promise
 
-function getuser(id){
+//getuser(1).then(user=>getrepo(user.gitusername)).then(repo=>console.log(repo)).catch(err=>console.log('Error',err.message));
+
+// Async and await
+async function displayrepo()
+{
+    try
+    {
+    const user= await getuser(1);
+    console.log(user)
+    const repo=await getrepo(user.username);
+    console.log(repo)
+    }
+    catch(err){
+        console.log(err.message)
+    }
+    
+}
+
+displayrepo()
+
+function getuser(id) {
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             console.log('Reading a user from DB') // Async non blocking function
@@ -23,11 +45,14 @@ function getuser(id){
     
 }
 
+
+
 function getrepo(username){
     return new Promise((resolve,reject)=>{
         setTimeout(() => {
             console.log('Getting repo information')
             resolve(['repo1','repo2','repo3'])
+            //reject(new Error('No repo found'))
         }, 2000);
     })
     
