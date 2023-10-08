c6f7cad2cc1af56b445c58fbda6e3dba19005968
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-07 17:41:51 -0700

    Callbacks in JS

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,14 +1,27 @@
 console.log('Before');
-const user=getuser(); // This will give undefined because the function can retrieve the answer in time. so inorder to get the answer we have to use callback or promises or async await
-console.log(user)
+getuser(1,(user)=>{
+    console.log(user) // when the result is ready the function will be executed and result is displayed -- async using callback
+    getrepo(user.gitusername,(repo)=>{
+        console.log(repo)
+    })
+}); 
 console.log('After'); // 2 nd one has to wait until first one is executed. Synchrous program
 
 
 
-function getuser(id){
+function getuser(id,callback){
     setTimeout(()=>{
         console.log('Reading a user from DB') // Async non blocking function
-        return {id:id,gitusername:'kausic'}
+        callback({id:id,gitusername:'kausic'})
+        //return {id:id,gitusername:'kausic'}
     },2000)
     
+}
+
+function getrepo(username,callback){
+    setTimeout(() => {
+        console.log('Getting repo information')
+        callback(['repo1','repo2','repo3'])
+    }, 2000);
+   
 }
