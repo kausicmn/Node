1319826c543a2c77501f2d9653b157f7cd29d836
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-10-07 18:25:49 -0700

    Running promise in parallel

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,5 +1,26 @@
 // const p=Promise.resolve({id:1})
 // p.then(res=>console.log(res))
 
-const p=Promise.reject(new Error('rejected'))
-p.then(err=>console.log(err.message))
+// const p=Promise.reject(new Error('rejected'))
+// p.then(err=>console.log(err.message))
+
+const p1= new Promise((resolve,reject)=>{
+setTimeout(()=>{
+    console.log('Async 1..')
+    //reject(new Error('some error'))
+    resolve(1);
+},2000)
+});
+
+const p2= new Promise((resolve)=>{
+    setTimeout(()=>{
+        console.log('Async 2..')
+        resolve(2)
+    },2000)
+    });
+
+///Promise.all([p1,p2]).then(res=>console.log(res)).catch(err=>console.log(err.message));
+
+Promise.race([p1,p2]).then(res=>console.log(res)).catch(err=>console.log(err.message));
+
+    
