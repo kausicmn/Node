6fcde7703a4e16a011dda3159d4749384eaf9e1a
kausicmn <92553246+kausicmn@users.noreply.github.com>
2024-03-03 22:06:44 -0500

    Sub documents embedding

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -14,13 +14,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author:authorSchema
+  authors:[authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -42,5 +42,27 @@ async function updateAuthor(courseId){
     }
   })
 }
-updateAuthor('65e523f4efceaac2d18f9775')
-//createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAuthor(courseId,author){
+  const course=await Course.findById(courseId)
+  course.authors.push(author)
+   await course.save()
+}
+async function removeAuthor(courseId,authorId){
+  const course=await Course.findById(courseId)
+  const authorIndex = course.authors.findIndex(author => author._id.equals(authorId));
+
+  if (authorIndex === -1) {
+    console.log('Author not found in the course');
+    return;
+  }
+
+  // Remove the author from the authors array
+  course.authors.splice(authorIndex, 1);
+
+  // Save the updated course
+  await course.save();
+}
+//updateAuthor('65e523f4efceaac2d18f9775')
+// createCourse('Node Course', [new Author({ name: 'Mosh' }),new Author({ name: 'Kausic' })]);
+//addAuthor('65e525f832d4a9cf1a240987',new Author({ name: 'Cloud' }))
+removeAuthor('65e525f832d4a9cf1a240987','65e528fb6e2645d35d72fd89')
